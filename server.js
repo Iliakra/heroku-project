f4a594483d66c52735988c7c9032f1e7a9d0a8c9
Ilya Krasilnikov <kravus27@gmail.com>
2021-05-18 17:01:36 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const Koa = require('koa');
+const app= new Koa();
+
+const tickets = [{id: 1, name: 'Поменять краску в принтере', status: true, created: '18/05/2021 13.15'}, {id: 2, name: 'Найти сотрудника', status: false, created: '18/05/2021 14.15'}];
+const ticketsFull = [{id: 1, name: 'Поменять краску в принтере', description: 'eeddedeededededed', status: true, created: '18/05/2021 13.15'}, {id: 2, name: 'Найти сотрудника', description: 'eeddedeededededed', status: false, created: '18/05/2021 14.15'}]
+
+app.use(async(ctx)=> {
+    const { method } = ctx.request.query;
+
+    switch (method) {
+        case 'allTickets':
+            ctx.response.body = tickets;
+            return;
+        case 'ticketById&id':
+            ctx.response.body = ticketsFull;
+            return;
+        default:
+            ctx.response.status = 404;
+            return;
+    }
+
+});
+
+const server = http.createServer(app.callback()).listen(7070);
