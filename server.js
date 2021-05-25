c359627eeaed055e8dd33d721e66912da222ff78
Iliakra <kravus27@gmail.com>
2021-05-25 20:50:09 +0300

    koa-body added

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const http = require('http');
 const Koa = require('koa');
+const koaBody = require('koa-body');
 const app= new Koa();
 
 const tickets = [{id: 1, name: 'Серверная задача 1', status: true, created: '18/05/2021 13.15'}, {id: 2, name: 'Найти сотрудника', status: false, created: '18/05/2021 14.15'}];
 const ticketsFull = [{id: 1, name: 'Серверная задача 1', description: 'eeddedeededededed', status: true, created: '18/05/2021 13.15'}, {id: 2, name: 'Найти сотрудника', description: 'eeddedeededededed', status: false, created: '18/05/2021 14.15'}]
+
+app.use(koaBody({urlencoded:true,}));
 
 app.use(async ctx => {
     const { method } = ctx.request.query;
@@ -25,6 +28,12 @@ app.use(async ctx => {
     }
 
 });
+
+app.use(async ctx => {
+    const requestBody = ctx.request.body;
+    ctx.response.set({'Access-Control-Allow-Origin':'*',});
+    ctx.response.body = requestBody;
+})
 
 
 const port = process.env.PORT||7070;
