d0b1a2b780fb61992542e0037fe0e5f68d6a65a4
Ilya Krasilnikov <kravus27@gmail.com>
2021-05-28 21:58:34 +0300

    eded

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const Koa = require('koa');
 const koaBody = require('koa-body');
 const app= new Koa();
 
-const tickets = [{id: 1, name: 'Серверная задача 1', status: true, created: '18.05.2021 13.15'}, {id: 2, name: 'Найти сотрудника', status: false, created: '18.05.2021 14.15'}];
-const ticketsFull = [{id: 1, name: 'Серверная задача 1', description: 'eeddedeededededed', status: true, created: '18.05.2021 13.15'}, {id: 2, name: 'Найти сотрудника', description: 'eeddedeededededed', status: false, created: '18.05.2021 14.15'}]
+const tickets = [
+    {id: 1, name: 'Серверная задача 1', status: "true", created: '18.05.2021 13.15'}, 
+    {id: 2, name: 'Найти сотрудника', status: "false", created: '18.05.2021 14.15'}
+];
+
+const ticketsFull = [
+    {id: 1, name: 'Серверная задача 1', description: 'eeddedeededededed', status: "true", created: '18.05.2021 13.15'}, 
+    {id: 2, name: 'Найти сотрудника', description: 'eeddedeededededed', status: "false", created: '18.05.2021 14.15'}
+]
 
 function displayTime() {
     var str = "";
