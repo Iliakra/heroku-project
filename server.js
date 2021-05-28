6f5f61876c216d69f724710bb073580b10cbca94
Ilya Krasilnikov <kravus27@gmail.com>
2021-05-28 14:24:44 +0300

    ed

--- server.js
+++ server.js
@@ -76,8 +76,7 @@ app.use(async ctx => {
             return
         case 'ticketById':
             const { id } = ctx.request.query;
-            console.log('id',id);
-            let ticket = findTicketById(id);
+            let ticket = findTicketById(Number(id));
             console.log('ticket',ticket);
             ctx.response.body = JSON.stringify(ticket);
             return
